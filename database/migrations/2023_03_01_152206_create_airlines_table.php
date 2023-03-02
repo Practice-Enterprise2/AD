6846fe941d40b71cad52bb17c663f05d032a38b2
Thomas Frans <franske2000@gmail.com>
2023-03-02 11:10:09 +0100

    Rename migrations.

--- database/migrations/2023_03_01_152206_create_airlines_table.php
+++ database/migrations/2023_03_01_152206_create_airlines_table.php
@@ -8,8 +8,10 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('airlines', function (Blueprint $table) {
             $table->id();
@@ -21,8 +23,10 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('airlines');
     }
