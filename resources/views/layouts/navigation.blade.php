8c1893f8da408b1da83ce64f9e2f619e34c4066f
Azety <tom.devyver@gmail.com>
2023-03-09 00:17:33 +0100

    navigation fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,59 +15,76 @@
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         {{ __('Home') }}
                     </x-nav-link>
-                    
+
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-
-                    <x-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
-                        {{ __('Profile') }}
-                    </x-nav-link>
-
-
-     
-
-
-
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+
+                @guest
+                <a class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                            href="{{ route('login') }}">{{ __('Login') }}</a>
+                                            <a class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                            href="{{ route('register') }}">{{ __('Register') }}</a>
+                @else
+
+
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
                             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
+                            @guest
+                                @if (Route::has('login'))
+                                        <a class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                            href="{{ route('login') }}">{{ __('Login') }}</a>
+                                @endif
+                                @if (Route::has('register'))
+                            
+                                        <a class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                            href="{{ route('register') }}">{{ __('Register') }}</a>
+                                
+                                @endif
+                            @else
+                                <div>
+                                {{ Auth::user()->name }}
+                                </div>
+                            @endguest
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                
                             </div>
                         </button>
                     </x-slot>
 
                     <x-slot name="content">
+                    @if (Route::has('login'))
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
-
+                    @endif
                         <!-- Authentication -->
+
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
+                            @guest
+                            @else
                             <x-dropdown-link :href="route('logout')"
                                 onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
+                            @endguest
                         </form>
                     </x-slot>
                 </x-dropdown>
+                @endguest
             </div>
 
             <!-- Hamburger -->
@@ -96,11 +113,17 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-
+                            @guest
+                                @if (Route::has('login'))
+                                @endif
+                                @if (Route::has('register'))
+                                @endif
+                            @else
+                                <div class="px-4">
+                                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{Auth::user()->name}}</div>
+                                    <div class="font-medium text-sm text-gray-500">{{Auth::user()->email}}</div>
+                                </div>
+                            @endguest
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
@@ -110,11 +133,14 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
+                    @guest
+                    @else
                     <x-responsive-nav-link :href="route('logout')"
                         onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
+                    @endguest
                 </form>
             </div>
         </div>
