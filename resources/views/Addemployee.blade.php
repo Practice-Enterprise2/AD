39e049bc38c6684b70cff3b7155e97a88b998209
nielsver <nielsvervoort14@gmail.com>
2023-03-09 12:12:27 +0100

    addemployee

---
+++ resources/views/Addemployee.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        @extends('layouts.header')
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/employee_add.css">
+        <title>Employee</title>
+    </head>
+    <>
+    @section('content') 
+        <h1>New Employee</h1>
+        <form method="post" action="employee_add" accept-charset="UTF-8">
+            @csrf
+            <div id="left">
+            <label for="firstName">First name:</label>
+            <input type="text" id="firstName" name="firstName" required><br>
+            <label for="lastName">Last name:</label>
+            <input type="text" id="lastName" name="lastName" required><br>
+            <label for="street">street:</label>
+            <input type="text" id="street" name="street" required><br>
+            <label for="province">province:</label>
+            <input type="text" id="province" name="province" required><br>
+            <label for="city">city:</label>
+            <input type="text" id="city" name="city" required><br>
+            <label for="postalCode">postalcode:</label>
+            <input type="number" id="postalCode" name="postalCode" required><br>
+            <label for="phonenumber">phonenumber:</label>
+            <input type="tel" id="phoneNumber" name="phoneNumber" required><br>
+            </div>
+            <div id="right">
+            <label for="mail">mail:</label>
+            <input type="email" id="mail" name="mail" required><br>
+            <label for="dateOfBirth">date of birth:</label>
+            <input type="date" id="dateOfBirth" name="dateOfBirth" required><br>
+            <label for="jobTitle">job title:</label>
+            <input type="text" id="jobTitle" name="jobTitle" required><br>
+            <label for="salary">salary:</label>
+            <input type="number" id="salary" name="salary" required><br>
+            <label for="password">password:</label>
+            <input type="password" id="password" name="password" required><br>
+            <label for="Iban">Iban:</label>
+            <input type="int" id="Iban" name="Iban" required><br>
+            <label for="isActive">active:</label>
+            <select name="isActive" id="isActive" value="yes">
+                <option value="yes">yes</option>
+                <option value="no">no</option>
+            </select>
+            </div>
+            <br>
+            <a href="/">Back</a>
+            <button type="reset">reset</button>
+            <button type="submit">New</button>
+        </form>
+        @endsection
+</html>
