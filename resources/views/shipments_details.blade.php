25ec7b7b00124ec79780f7f7b5619556c11a4b81
GillesWS <gilles.waegemans@student.thomasmore.be>
2023-03-23 10:52:45 +0100

    Redesign shipment detail page

--- resources/views/shipments_details.blade.php
+++ resources/views/shipments_details.blade.php
@@ -3,22 +3,36 @@
 
     @if(count($shipments) > 0)
     @foreach($shipments as $shipment)
-    <div class="text-center">
-    <h1 class="text-2xl">Tracking details</h1>
-    <p>Package of: <strong>{{$shipment->ShipmentName}}</strong></p>
-    <p>Created on: <strong>{{$shipment->ShipmentDate}}</strong></p>
-    <p>Delivery on: <strong>{{$shipment->DeliveryDate}}</strong></p>
-    <p>Weight of: <strong>{{$shipment->ShipmentWeight}}</strong></p>
-    <p>Status: <strong>{{$shipment->Name}}</strong></p>
-    </div>
-    @endforeach
-    @else
-    <div class="row">
-        <div class="col s12">
-            <div class="card">
-                <p>No posts found</p>
-            </div>
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h1 class="text-2xl font-semibold mb-4">{{ __('Shipment Tracking') }}</h1>
+        <div class="flex justify-between mb-8">
+          <div>
+            <p class="text-sm font-medium text-gray-500">{{ __('Shipment Date') }}</p>
+            <p class="text-lg font-semibold">{{ $shipment->ShipmentDate }}</p>
+          </div>
+          <div>
+            <p class="text-sm font-medium text-gray-500">{{ __('Estimated Delivery') }}</p>
+            <p class="text-lg font-semibold">{{ $shipment->DeliveryDate }}</p>
+          </div>
+          <div>
+            <p class="text-sm font-medium text-gray-500">{{ __('Shipment Weight') }}</p>
+            <p class="text-lg font-semibold">{{ $shipment->ShipmentWeight }} kg</p>
+          </div>
+          <div>
+            <p class="text-sm font-medium text-gray-500">{{ __('Status') }}</p>
+            <p class="text-lg font-semibold {{ $shipment->Name === 'Completed' ? 'text-green-600' : 'text-orange-600' }}">{{ $shipment->Name }}</p>
+          </div>
+        </div>
+        <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+          <div class="px-6 py-4">
+            <h2 class="text-xl font-semibold mb-4">{{ __('Sender Information') }}</h2>
+            <p class="text-gray-700 mb-2"><span class="font-medium">{{ __('Name') }}:</span> {{ $shipment->ShipmentName }}</p>
+          </div>
         </div>
     </div>
+    <div class="text-center pt-6">
+        <a href="../shipments"><button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Back</button></a>
+    </div>
+    @endforeach
     @endif
 </x-app-layout>
