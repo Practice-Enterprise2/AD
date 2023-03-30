0f1e07bf9f043038257f274533324e21083ce5ef
Wanniox <wannes.3h@gmail.com>
2023-03-30 10:50:08 +0200

    created edit table

---
+++ database/migrations/2023_03_30_080144_create_edits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+        Schema::dropIfExists('edits');
+        
+        Schema::create('edits', function (Blueprint $table) {
+            $table->id();
+            $table->string('soortEdit', 24);
+            $table->string('tableEdited', 24);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('edits');
+    }
+};
