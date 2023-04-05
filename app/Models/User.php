8a94028a4985e408431406b6eb416d4ccf049112
Thomas Frans <franske2000@gmail.com>
2023-04-05 21:54:46 +0200

    Database migration for users table.
    Add address information and last name fields to users table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Http\Response;
@@ -14,12 +16,36 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    public function roles()
+    protected $fillable = [
+        'name',
+        'last_name',
+        'email',
+        'email_verified_at',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class);
     }
-
-    public function checkRoles($roles)
+    /**
+     * @param mixed $roles
+     */
+    public function checkRoles($roles): void
     {
         if (! is_array($roles)) {
             $roles = [$roles];
@@ -29,45 +55,18 @@ class User extends Authenticatable implements MustVerifyEmail
             abort(Response::HTTP_NOT_FOUND);
         }
     }
-
+    /**
+     * @param mixed $roles
+     */
     public function hasAnyRole($roles): bool
     {
         return (bool) $this->roles()->whereIn('name', $roles)->first();
     }
-
+    /**
+     * @param mixed $role
+     */
     public function hasRole($role): bool
     {
         return (bool) $this->roles()->where('name', $role)->first();
     }
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'email_verified_at',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
