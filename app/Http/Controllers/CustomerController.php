d4a060fdcd679b86fe8b5c721024f4d3b720d314
jentevs <vansanjente@gmail.com>
2023-04-06 12:24:14 +0200

    customer edit form lifewire

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,57 +5,73 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Address;
-use Livewire\Components\CustomerForm;
+use Livewire\Components;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
     public function getCustomers()
-    {
-        $users = User::whereNotIn('id', function ($query) {
-                $query->select('user_id')
-                      ->from('employees');
-            })->get();
+{
+    $users = User::whereNotIn('id', function ($query) {
+            $query->select('user_id')
+                  ->from('employees');
+        })->get();
 
-        foreach ($users as $user) {
-            $address = Address::find($user->address_id);
-            $user->address = $address;
-        }
-
-        return view('customers', ['users' => $users]);
+    foreach ($users as $user) {
+        $address = Address::find($user->address_id);
+        $user->address = $address;
     }
 
-    public function edit($id)
-    {
-        $customer = User::find($id);
-        $address = Address::find($customer->address_id);
+    return view('customers', ['users' => $users]);
+}
+public function edit($id)
+{
 
-        return view('customers_edit', [
-            'customer' => $customer,
-            'address' => $address,
-            'component' => app()->make(CustomerForm::class, ['id' => $id]),
-        ]);
+    $customer = User::find($id);
+    $address = Address::find($customer->address_id);
+
+    return view('customers_edit', ['customer' => $customer, 'address' => $address]);
+}
+
+public function update(Request $request, $id)
+{
+    $customer = User::find($id);
+    $address = Address::find($customer->address_id);
+
+    
+    $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'last_name' => 'required',
+        'phone' => 'required',
+        'street' => 'required',
+        'house_number' => 'required',
+        'postal_code' => 'required',
+        'city' => 'required',
+        'region' => 'required',
+        'country' => 'required',
+    ]);
+    
+    if ($validator->fails()) {
+        return redirect()->back()->withErrors($validator)->withInput();
     }
+    
+    $customer->name = $request->input('name');
+    $customer->last_name = $request->input('last_name');
+    $customer->email = $request->input('email');
+    $customer->phone = $request->input('phone');
+    
+    $address->street = $request->input('street');
+    $address->house_number = $request->input('house_number');
+    $address->postal_code = $request->input('postal_code');
+    $address->city = $request->input('city');
+    $address->region = $request->input('region');
+    $address->country = $request->input('country');
+    
+    $customer->save();
+    $address->save();
+    
+    return redirect()->route('customer.edit', $id)->with('success', 'Customer updated successfully.');
+    
 
-    public function update(Request $request, $id)
-    {
-        $customer = User::find($id);
-        $address = Address::find($customer->address_id);
-
-        $customer->name = $request->input('name');
-        $customer->last_name = $request->input('last_name');
-        $customer->email = $request->input('email');
-        $customer->phone = $request->input('phone');
-
-        $address->street = $request->input('street');
-        $address->house_number = $request->input('house_number');
-        $address->postal_code = $request->input('postal_code');
-        $address->city = $request->input('city');
-        $address->region = $request->input('region');
-        $address->country = $request->input('country');
-
-        $customer->save();
-        $address->save();
-
-        return redirect('/customers');
-    }
+}
 }
--- resources/views/customers_edit.blade.php
+++ resources/views/customers_edit.blade.php
@@ -55,11 +55,11 @@ tr{
 <table>
     <tr>
         <td><label for="name">Name</label></td>
-        <td><input type="text" name="name" id="name" value="{{ $customer->name }}"></td>
+        <td><input type="text" name="name" id="name" value="{{ $customer->name }}"> @error('name') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>
     <tr>
         <td><label for="last_name">last_name</label></td>
-        <td><input type="text" name="last_name" id="last_name" value="{{ $customer->last_name }}"></td>
+        <td><input type="text" name="last_name" id="last_name" value="{{ $customer->last_name }}">@error('last_name') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>
     <tr>
         <td><label for="email">Email</label></td>
@@ -67,34 +67,34 @@ tr{
     </tr>
     <tr>
         <td><label for="phone">Phone</label></td>
-        <td><input type="text" name="phone" id="phone" value="{{ $customer->phone }}"></td>
+        <td><input type="text" name="phone" id="phone" value="{{ $customer->phone }}">@error('phone') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>
 
     <tr>
         <td><label for="street">street</label></td>
-        <td><input type="text" name="street" id="street" value="{{ $customer->address->street }}"></td>
+        <td><input type="text" name="street" id="street" value="{{ $customer->address->street }}">@error('street') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>  
 
 
     <tr>
         <td><label for="house_number">house_number</label></td>
-        <td><input type="text" name="house_number" id="house_number" value="{{ $customer->address->house_number }}"></td>
+        <td><input type="text" name="house_number" id="house_number" value="{{ $customer->address->house_number }}">@error('house_number') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>  
     <tr>
         <td><label for="postal_code">postal_code</label></td>
-        <td><input type="text" name="postal_code" id="postal_code" value="{{ $customer->address->postal_code }}"></td>
+        <td><input type="text" name="postal_code" id="postal_code" value="{{ $customer->address->postal_code }}">@error('postal_code') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>  
     <tr>
         <td><label for="city">city</label></td>
-        <td><input type="text" name="city" id="city" value="{{ $customer->address->city }}"></td>
+        <td><input type="text" name="city" id="city" value="{{ $customer->address->city }}">@error('city') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>  
     <tr>
         <td><label for="region">region</label></td>
-        <td><input type="text" name="region" id="region" value="{{ $customer->address->region }}"></td>
+        <td><input type="text" name="region" id="region" value="{{ $customer->address->region }}">@error('region') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>  
     <tr>
         <td><label for="country">country</label></td>
-        <td><input type="text" name="country" id="country" value="{{ $customer->address->country }}"></td>
+        <td><input type="text" name="country" id="country" value="{{ $customer->address->country }}">@error('country') <div class="text-danger">{{ $message }}</div>@enderror</td>
     </tr>  
     
 
