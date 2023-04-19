9abf17960418885b3a62cbbe755814dca716874b
Jellios <62890668+Jellios@users.noreply.github.com>
2023-04-19 14:34:22 +0200

    fixed styling

--- resources/views/contract.blade.php
+++ resources/views/contract.blade.php
@@ -20,7 +20,7 @@ if (!isset($_GET["q"]))
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href=" {{  asset('css/new_contract.css')}}">
     <title>view records</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
