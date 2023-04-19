c7c52929a9f2231f632dc9dab161e67b3bebaaa7
guillaume848 <75422493+guillaume848@users.noreply.github.com>
2023-04-19 19:26:21 +0200

    filtering

--- app/Http/Controllers/contractListController.php
+++ app/Http/Controllers/contractListController.php
@@ -21,10 +21,29 @@ class contractlistcontroller extends Controller
     public function contractFiltering(Request $request)
     {
         $filter = $request->query('filter');
-    
+        $filter1 = $request->query('filter1');
+        $filter2 = $request->query('filter2');
         if (!empty($filter)) {
             $contracts = contractlist::sortable()
-                ->where('airline_id', 'like', '%'.$filter.'%')
+                ->where('id', 'like', '%'.$filter.'%')
+                ->paginate(15);
+        } else {
+            $contracts = contractlist::sortable()
+                ->paginate(15);
+        }
+
+        if (!empty($filter1)) {
+            $contracts = contractlist::sortable()
+                ->where('depart_location', 'like', '%'.$filter1.'%')
+                ->paginate(15);
+        } else {
+            $contracts = contractlist::sortable()
+                ->paginate(15);
+        }
+
+        if (!empty($filter2)) {
+            $contracts = contractlist::sortable()
+                ->where('destination_location', 'like', '%'.$filter2.'%')
                 ->paginate(15);
         } else {
             $contracts = contractlist::sortable()
