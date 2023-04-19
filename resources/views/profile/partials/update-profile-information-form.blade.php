bbe5b9464bc2fb400da0aaee70221a172017c974
Sam Al-Rahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-04-19 13:27:35 +0200

    Verify customer change
    * user has to verify before when changing info
    
    * user has to verify when changing his info

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -33,28 +33,6 @@
         class="mt-1 block w-full" :value="old('email', $user->email)" required
         autocomplete="username" />
       <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-      @if (
-          $user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&
-              !$user->hasVerifiedEmail())
-        <div>
-          <p class="mt-2 text-sm text-gray-800 dark:text-gray-200">
-            {{ __('Your email address is unverified.') }}
-
-            <button form="send-verification"
-              class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
-              {{ __('Click here to re-send the verification email.') }}
-            </button>
-          </p>
-
-          @if (session('status') === 'verification-link-sent')
-            <p
-              class="mt-2 text-sm font-medium text-green-600 dark:text-green-400">
-              {{ __('A new verification link has been sent to your email address.') }}
-            </p>
-          @endif
-        </div>
-      @endif
     </div>
 
     <div class="flex items-center gap-4">
@@ -67,5 +45,26 @@
         </p>
       @endif
     </div>
+    @if (
+        $user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&
+            !$user->hasVerifiedEmail())
+      <div>
+        <p class="mt-2 text-sm text-gray-800 dark:text-gray-200">
+          {{ __('Your email address is unverified.') }}
+
+          <button form="send-verification"
+            class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800">
+            {{ __('Click here to re-send the verification email.') }}
+          </button>
+        </p>
+
+        @if (session('status') === 'verification-link-sent')
+          <p
+            class="mt-2 text-sm font-medium text-green-600 dark:text-green-400">
+            {{ __('A new verification link has been sent to your email address.') }}
+          </p>
+        @endif
+      </div>
+    @endif
   </form>
 </section>
