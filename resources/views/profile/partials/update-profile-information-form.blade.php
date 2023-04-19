3584e93e55ed6b0dc276fad7eef9d42536f5b25b
SamAlrahhal <95586168+SamAlrahhal@users.noreply.github.com>
2023-04-19 10:32:06 +0200

    user has to verify before when changing info

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -14,6 +14,7 @@
     @csrf
   </form>
 
+
   <form method="post" action="{{ route('profile.update') }}"
     class="mt-6 space-y-6">
     @csrf
@@ -33,8 +34,19 @@
         class="mt-1 block w-full" :value="old('email', $user->email)" required
         autocomplete="username" />
       <x-input-error class="mt-2" :messages="$errors->get('email')" />
+    </div>
 
-      @if (
+    <div class="flex items-center gap-4">
+      <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+      @if (session('status') === 'profile-updated')
+        <p x-data="{ show: true }" x-show="show" x-transition
+          x-init="setTimeout(() => show = false, 2000)"
+          class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}
+        </p>
+      @endif
+    </div>
+          @if (
           $user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail &&
               !$user->hasVerifiedEmail())
         <div>
@@ -55,17 +67,5 @@
           @endif
         </div>
       @endif
-    </div>
-
-    <div class="flex items-center gap-4">
-      <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-      @if (session('status') === 'profile-updated')
-        <p x-data="{ show: true }" x-show="show" x-transition
-          x-init="setTimeout(() => show = false, 2000)"
-          class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}
-        </p>
-      @endif
-    </div>
   </form>
 </section>
