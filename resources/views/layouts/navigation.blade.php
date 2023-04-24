491c2b80f183a2f57922f8b83524408661d00e54
nielsver <nielsvervoort14@gmail.com>
2023-04-24 16:56:40 +0200

    placing of contact us

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -94,7 +94,6 @@
           </x-slot>
         </x-dropdown>
       @endauth
-
       @auth
         <x-dropdown>
           <x-slot name="trigger">
@@ -136,6 +135,50 @@
                 </x-dropdown-link>
               </a>
             @endif
+          </x-slot>
+        </x-dropdown>
+      @endauth
+      @auth
+        {{-- Validations needed for later. --}}
+        {{-- <x-nav-link :href="route('shipments.create')">
+        {{ __('Request Shipment') }}
+      </x-nav-link>
+      <x-nav-link :href="route('shipments.requests')">
+        {{ __('Evaluate Shipment Requests') }}
+      </x-nav-link>
+      <x-nav-link :href="route('shipments.index')">
+        {{ __('Show Confirmed Shipments') }}
+      </x-nav-link> --}}
+        <x-dropdown>
+          <x-slot name="trigger">
+            <button
+              class="mt-4 inline-flex items-center rounded-md border border-transparent border-white bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none dark:bg-gray-800 dark:text-gray-400 dark:hover:text-gray-300">
+              Contact us
+              <div class="ml-1">
+                <svg class="h-4 w-4 fill-current" viewBox="0 0 20 20">
+                  <path fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+                </svg>
+
+              </div>
+            </button>
+          </x-slot>
+
+          <x-slot name="content">
+            @if(Auth::user()->role == 0)
+            <x-dropdown-link :href="route('contact.create')">
+              {{ __('contact us') }}
+            </x-dropdown-link>
+            @endif
+            @if(Auth::user()->role == 1)
+            <x-dropdown-link :href="route('contact.index')">
+              {{ __('complaints') }}
+            </x-dropdown-link>
+            @endif
+            <x-dropdown-link :href="route('complaints.messages')">
+              {{ __('messages') }}
+            </x-dropdown-link>
           </x-slot>
         </x-dropdown>
       @endauth
