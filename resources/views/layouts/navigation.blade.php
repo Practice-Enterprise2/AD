82d0ba882b602333d9046c1d3a8d78d8e6f35fb6
nielsver <nielsvervoort14@gmail.com>
2023-04-26 17:42:13 +0200

    readreviews

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -138,6 +138,11 @@
         </x-dropdown>
       @endauth
       @auth
+            <x-nav-link :href="route('readreviews')" :active="request()->routeIs('readreviews')">
+              {{ __('Reviews') }}
+            </x-nav-link>
+      @endauth
+      @auth
         {{-- Validations needed for later. --}}
         {{-- <x-nav-link :href="route('shipments.create')">
         {{ __('Request Shipment') }}
