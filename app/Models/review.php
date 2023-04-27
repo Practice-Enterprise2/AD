b3e12c49adeb05d9d92e8171c878a2917a332122
nielsver <nielsvervoort14@gmail.com>
2023-04-27 09:28:50 +0200

    namingconventions

--- app/Models/review.php
+++ app/Models/review.php
@@ -2,20 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class review extends Model
+/**
+ * @property int $id
+ * @property int $rating
+ * @property string $comment
+ * @property ?\Illuminate\Support\Carbon $created_at
+ * @property ?\Illuminate\Support\Carbon $updated_at
+ */
+class Review extends Model
 {
-    use HasFactory;
-
-    /**
-     * Attributes to guard against mass-assignment.
-     *
-     * @var array
-     */
-    protected $guarded = [];
-
     protected $fillable = [
         'rating',
         'comment',
