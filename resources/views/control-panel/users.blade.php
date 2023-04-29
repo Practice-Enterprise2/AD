00166d5df7e5618bb3e2194c4b8f3db13f6e700d
nielsver <nielsvervoort14@gmail.com>
2023-04-29 15:47:29 +0200

    main

---
+++ resources/views/control-panel/users.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+  <x-control-panel-layout>
+    <x-slot:title>{{ __('Users') }}</x-slot:title>
+
+    @canany(['view_all_users', 'view_all_roles', 'edit_any_user_info'])
+      <div class="mt-5">
+        @livewire('users')
+      </div>
+      <div class="mt-5">
+        <h2 class="text-xl font-bold">{{ __('User Management Pages') }}</h2>
+        <div class="p-6 text-gray-900 dark:text-gray-100">
+          <ol>
+            @can(['view_all_users', 'edit_any_user_info'])
+              <li><a href="{{ route('users') }}">Users ></a></li>
+            @endcan
+            @can(['view_all_users', 'edit_any_user_info'])
+              <li><a href="{{ route('customers') }}">Customers ></a></li>
+            @endcan
+            @can(['view_all_roles', 'edit_roles'])
+              <li><a href="{{ route('roles') }}">Roles ></a></li>
+            @endcan
+          </ol>
+        </div>
+      </div>
+    @endcanany
+
+  </x-control-panel-layout>
+</x-app-layout>
+{{-- vim: ft=html
+--}}
---
+++ resources/views/livewire/roles.blade.php
@@ -0,0 +1,24 @@
+<div>
+  <input name="search" type="text" wire:model="search" autocomplete="off"
+    placeholder="Search">
+  <table class="w-full">
+    <tr class="text-left">
+      <th wire:click="sort_by('name')"
+        class="border hover:cursor-pointer dark:border-white">Name</th>
+      <th class="border dark:border-white">Description</th>
+      <th class="border dark:border-white"></th>
+    </tr>
+    @foreach ($roles as $role)
+      <tr>
+        <td class="border dark:border-white">{{ $role->name }}</td>
+        <td class="border dark:border-white">{{ $role->description }}</td>
+        <td class="border text-right dark:border-white"><a
+            href="{{ route('control-panel.roles.edit', ['role' => $role->id]) }}">Edit</a>
+        </td>
+      </tr>
+    @endforeach
+  </table>
+  {{ $roles->links() }}
+</div>
+{{-- vim: ft=html
+--}}
