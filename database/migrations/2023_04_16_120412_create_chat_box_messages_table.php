df7bb290d4accda3f6ca15ccdd192e96210ac3a4
nghidaide012 <nghitrn135@yahoo.com>
2023-04-29 13:44:43 +0200

    change down() migration file in customer_contact and chatbox_messages

--- database/migrations/2023_04_16_120412_create_chat_box_messages_table.php
+++ database/migrations/2023_04_16_120412_create_chat_box_messages_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('chat_box_messages');
     }
 };
--- database/migrations/2023_04_18_153939_customer_contacts.php
+++ database/migrations/2023_04_18_153939_customer_contacts.php
@@ -28,5 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('customer_contacts');
     }
 };
