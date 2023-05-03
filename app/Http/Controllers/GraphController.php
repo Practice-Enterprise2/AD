4c060b181955d0e5deeafc256ffd10d7a66c8cef
nielsver <nielsvervoort14@gmail.com>
2023-05-03 11:13:25 +0200

    update

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Employee;
+use App\Models\EmployeeContract;
 use Illuminate\Support\Carbon;
 
 class GraphController extends Controller
@@ -10,16 +10,15 @@ class GraphController extends Controller
     public function index()
     {
         $now = Carbon::now();
-        $data = Employee::selectRaw('MONTH(employee_contracts.start_date) as month, COUNT(*) as count')
-        ->join('employee_contracts', 'employee_contracts.employee_id', '=', 'employees.id')
-        ->where('employee_contracts.start_date', '<=', $now)
-        ->where(function ($query) use ($now) {
-            $query->where('employee_contracts.end_date', '>=', $now)
-                ->orWhereNull('employee_contracts.end_date');
-        })
-        ->groupBy('month')
-        ->get();
-
+        $data = EmployeeContract::selectRaw('MONTH(start_date) as month, COUNT(*) as count')
+            ->where('start_date', '<=', $now)
+            ->where(function ($query) use ($now) {
+                $query->where('end_date', '>=', $now)
+                    ->orWhereNull('end_date');
+            })
+            ->groupBy('month')
+            ->get();
+            
         $countpm = [];
         for ($i = 1; $i <= 12; $i++) {
             $monthData = $data->where('month', $i)->first();
