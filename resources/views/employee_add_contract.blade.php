9a06ee2b4173fbd20dd8f5620f49eada3fd91f20
ParkwayBE <101404183+ParkwayBE@users.noreply.github.com>
2023-05-04 00:02:27 +0200

    commit again

--- resources/views/employee_add_contract.blade.php
+++ resources/views/employee_add_contract.blade.php
@@ -1,27 +1,31 @@
 <x-app-layout>
-<div class="text-center bg-white w-2/5 m-auto">
-  <h1 class="text-center text-xl p-8">New Employee Contract</h1>
-  
-  <form method="post" action="employee_add_contract_done" accept-charset="UTF-8">
-    @csrf
-    
-    <label for="employeeID">Select an employee:</label>
-    <select name="employeeID" id="employeeID" required>
-    @foreach ($users as $user)
-      <option value="{{$user['employeeID']}}">{{$user['name']}} {{$user['lastname']}}</option>
-    @endforeach
-    </select>
-    <br>
-    
-    <label for="startdate">Start date:</label>
-    <input type="date" id="startdate" name="startdate" required><br>
-    <label for="stopdate">Stop date:</label>
-    <input type="date" id="stopdate" name="stopdate" required><br>
+  <div class="m-auto w-2/5 bg-white text-center">
+    <h1 class="p-8 text-center text-xl">New Employee Contract</h1>
 
-    <br>
+    <form method="post" action="employee_add_contract_done"
+      accept-charset="UTF-8">
+      @csrf
 
-    <button type="reset" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">reset</button>
-    <button type="submit" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add</button>
-  </form>
+      <label for="employeeID">Select an employee:</label>
+      <select name="employeeID" id="employeeID" required>
+        @foreach ($users as $user)
+          <option value="{{ $user['employeeID'] }}">{{ $user['name'] }}
+            {{ $user['lastname'] }}</option>
+        @endforeach
+      </select>
+      <br>
+
+      <label for="startdate">Start date:</label>
+      <input type="date" id="startdate" name="startdate" required><br>
+      <label for="stopdate">Stop date:</label>
+      <input type="date" id="stopdate" name="stopdate" required><br>
+
+      <br>
+
+      <button type="reset"
+        class="mb-2 mr-2 rounded-full bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700">reset</button>
+      <button type="submit"
+        class="mb-2 mr-2 rounded-full bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700">Add</button>
+    </form>
   </div>
 </x-app-layout>
--- resources/views/employee_view_contracts.blade.php
+++ resources/views/employee_view_contracts.blade.php
@@ -1,19 +1,23 @@
 <x-app-layout>
-  <h1 class="text-center text-xl p-8">Employee Contracts</h1>
+  <h1 class="p-8 text-center text-xl">Employee Contracts</h1>
   <div class="text-center">
-    <a id="newemployee" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"  href="{{ route('create-contract-index') }}">new contract</a>
-    </div> 
-    @foreach ($contracts as $contract)
-    <div class="text-center bg-white w-2/5 m-auto">
+    <a id="newemployee"
+      class="mb-2 mr-2 rounded-full bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+      href="{{ route('create-contract-index') }}">new contract</a>
+  </div>
+  @foreach ($contracts as $contract)
+    <div class="m-auto w-2/5 bg-white text-center">
 
-        <p>contract ID: {{ $contract["contractID"] }}</p>
-        <p>employee id: {{ $contract["employeeID"] }}</p>
-        <p>first name: {{ $contract["name"] }}</p>
-        <p>last name: {{ $contract["lastname"] }}</p>
-        
-        <p>start date: {{ $contract["startdate"] }}</p>
-        <p>end date: {{ $contract["enddate"] }}</p>
-        <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">download contract</button>
+      <p>contract ID: {{ $contract['contractID'] }}</p>
+      <p>employee id: {{ $contract['employeeID'] }}</p>
+      <p>first name: {{ $contract['name'] }}</p>
+      <p>last name: {{ $contract['lastname'] }}</p>
+
+      <p>start date: {{ $contract['startdate'] }}</p>
+      <p>end date: {{ $contract['enddate'] }}</p>
+      <button
+        class="mb-2 mr-2 rounded-full bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700">download
+        contract</button>
     </div>
-    @endforeach
+  @endforeach
 </x-app-layout>
