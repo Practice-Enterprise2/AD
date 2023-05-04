5e1133d9aeee07f5df985159f479911e840f6a33
Biomoz <natecelebi@gmail.com>
2023-05-04 11:38:02 +0200

    changed the waypoint validations as well

--- app/Models/Waypoint.php
+++ app/Models/Waypoint.php
@@ -22,8 +22,29 @@ class Waypoint extends Model implements ValidatesAttributes
 
     public const VALIDATION_RULE_STATUS = ['required', 'in:In Transit,Out For Delivery,Delivered,Exception'];
 
+    public const VALIDATION_RULE_STREET = ['required', 'string', 'regex:/^[A-Za-z0-9\s]+$/'];
+
+    public const VALIDATION_RULE_HOUSENUMBER = ['required', 'string'];
+
+    public const VALIDATION_RULE_POSTALCODE = ['required', 'string', 'regex:/^[A-Za-z0-9\s]+$/'];
+
+    public const VALIDATION_RULE_CITY = ['required', 'string', 'regex:/^[A-Za-z\s]+$/'];
+
+    public const VALIDATION_RULE_REGION = ['required', 'string', 'regex:/^[A-Za-z\s]+$/'];
+
+    public const VALIDATION_RULE_COUNTRY = ['required', 'string', 'regex:/^[A-Za-z\s]+$/'];
+
+    public const VALIDATION_RULE_ARRAY = ['required', 'array'];
+
     public const VALIDATION_RULES = [
+        'array' => self::VALIDATION_RULE_ARRAY,
         'status' => self::VALIDATION_RULE_STATUS,
+        'street' => self::VALIDATION_RULE_STREET,
+        'house_number' => self::VALIDATION_RULE_HOUSENUMBER,
+        'postal_code' => self::VALIDATION_RULE_POSTALCODE,
+        'city' => self::VALIDATION_RULE_CITY,
+        'region' => self::VALIDATION_RULE_REGION,
+        'country' => self::VALIDATION_RULE_COUNTRY,
     ];
 
     protected $fillable = [
