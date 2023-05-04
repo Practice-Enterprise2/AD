f17afdfe15df7fd43f61192e018dc7b77358c4e6
WillemDC <willemdc1@gmail.com>
2023-05-04 11:51:43 +0200

    Some frontend validations for airport forms

--- resources/views/addAirportList.blade.php
+++ resources/views/addAirportList.blade.php
@@ -1,19 +1,19 @@
 <h1>Add airport</h1>
 
-<form action="airportList/add" method="POST">
+<form action="airportList/add" method="POST" autocomplete="off">
    @csrf
 
   <label for="iata_code">IATA code</label> <br>
-  <input type="iata_code" name="iata_code"> <br> <br>
+  <input type="text" maxlength="3" name="iata_code" required> <br> <br>
 
   <label for="name">Airport Name</label> <br>
-  <input type="text" name="name"> <br> <br>
+  <input type="text" name="name" required> <br> <br>
 
   <label for="land">Country</label> <br>
-  <input type="text" name="land"> <br> <br>
+  <input type="text" name="land" required> <br> <br>
 
   <label for="address_id">Address ID</label> <br>
-  <input type="number" name="address_id"> <br> <br>
+  <input type="number" maxlength="20" name="address_id" required> <br> <br>
 
   <button type="submit">Add airport</button>
 </form>
--- resources/views/editAirportList.blade.php
+++ resources/views/editAirportList.blade.php
@@ -2,19 +2,19 @@
 
 <form action="/editAirport" method="POST">
     @csrf
-    <input type="hidden" name="id" value="{{ $data['id'] }}">
+    <input type="hidden" name="id" value="{{ $data['id'] }}" required>
 
     <label for="iata_code">IATA code</label> <br>
-    <input type="iata_code" name="iata_code" value="{{ $data['iata_code'] }}"> <br> <br>
+    <input type="text" maxlength="3" name="iata_code" value="{{ $data['iata_code'] }}" required> <br> <br>
 
     <label for="name">Airport Name</label> <br>
-    <input type="text" name="name" value="{{ $data['name'] }}"> <br> <br>
+    <input type="text" name="name" value="{{ $data['name'] }}" required> <br> <br>
     
     <label for="land">Country</label> <br>
-    <input type="text" name="land" value="{{ $data['land'] }}"> <br> <br>
+    <input type="text" name="land" value="{{ $data['land'] }}" required> <br> <br>
 
     <label for="address_id">Address ID</label> <br>
-    <input type="number" name="address_id" value="{{ $data['address_id'] }}"> <br> <br>
+    <input type="number" maxlength="20" name="address_id" value="{{ $data['address_id'] }}" required> <br> <br>
 
     <button type="submit">Update</button>
 </form>
