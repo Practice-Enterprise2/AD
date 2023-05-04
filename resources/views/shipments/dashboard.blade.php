ee5e20309e099fead1a8098c97b9baee275e98bf
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2023-05-04 11:31:57 +0200

    status overview

--- resources/views/shipments/dashboard.blade.php
+++ resources/views/shipments/dashboard.blade.php
@@ -1,3 +1,84 @@
 <x-app-layout>
-    
+    <div class="flex-grow p-8 dark:bg-gray-900 dark:text-white">
+        <h1 class="text-2xl font-bold mb-8">Dashboard</h1>
+      
+        <div class="flex mb-8">
+          <div class="w-1/3 p-4 bg-gray-800 rounded shadow-lg mr-4 text-lg font-bold">User Shipments
+            <div class="text-sm font-normal">555</div>
+          </div>
+          <div class="w-1/3 p-4 bg-gray-800 rounded shadow-lg mr-4">
+            <div class="grid grid-cols-2 gap-1 text-sm">
+                <h1 class="col-span-2 p-4 text-white font-bold text-lg">Shipments per status</h1>
+                <div class="mx-auto">
+                  <div class="">Awaiting pickup</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Awaiting confirmation</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">In Transit</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Out for Delivery</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Delivered</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Held at location</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Exeption</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Deleted</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">Declined</div>
+                </div>
+                <div class="mx-auto">
+                    <div class="">1</div>
+                </div>
+              </div>
+              
+          </div>
+          <div class="w-1/3 mx-quto bg-gray-800 rounded shadow-lg text-lg font-bold">Total To Pay
+            <div class="text-sm font-normal">44</div>
+          </div>
+        </div>
+      
+        <div class="flex mb-8">
+          <div class="w-1/2 p-4 bg-gray-800 rounded shadow-lg mr-4">Your Invoices
+          </div>
+          <div class="w-1/2 p-4 bg-gray-800 rounded shadow-lg">Panel 5</div>
+        </div>
+      
+      </div>
+      
 </x-app-layout>
+
+
