3a580273a8a752cbe5db3db9563088b87b7aceed
johan525 <95874895+johan525@users.noreply.github.com>
2023-05-10 19:47:50 +0200

    Update 2023_05_10_170519_update_employees.php

--- database/migrations/2023_05_10_170519_update_employees.php
+++ database/migrations/2023_05_10_170519_update_employees.php
@@ -22,7 +22,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            //
         });
     }
 };
