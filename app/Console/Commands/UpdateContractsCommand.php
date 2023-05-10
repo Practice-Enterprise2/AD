dc21dfd10b7ff03109b1b700fe623fb2520b1bcd
johan525 <95874895+johan525@users.noreply.github.com>
2023-05-10 19:46:24 +0200

    Update UpdateContractsCommand.php

--- app/Console/Commands/UpdateContractsCommand.php
+++ app/Console/Commands/UpdateContractsCommand.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Carbon\Carbon;
 
 class UpdateContractsCommand extends Command
 {
@@ -11,20 +14,23 @@ class UpdateContractsCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:update-contracts-command';
+    protected $signature = 'contracts:update';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Updata contracts that have run out by setting them to inactive';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $today = Carbon::today();
+        DB::table('contacts')
+            ->where('end_date',$today)
+            ->update(['active' => false]);
     }
 }
