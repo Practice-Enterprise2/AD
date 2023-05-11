54a4d9fab32f0363b8ac12644d15381af0c8606a
nielsver <nielsvervoort14@gmail.com>
2023-05-11 14:29:44 +0200

    format

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,38 +1,62 @@
 <x-app-layout>
-<div class="flex items-center justify-center">
-    <div class="mx-auto grid grid-cols-4 md:grid-cols-4 gap-8 justify-center items-center" style="height: 80vh; width: 80vw;">
-    @auth
-      <x-canvas-tile title="Dashboard" url="/dashboard" description="To the dashboard" ></x-canvas-tile>
-    @endauth
-    @can('view_general_employee_content')
-      <x-canvas-tile title="Employee" url="/employee" description="Explore and search for employees in the company"></x-canvas-tile>
-    @endcan
-    @can('view_reviews')
-      <x-canvas-tile url="/readreviews" title="Customer Complaints" description="Stay informed about all customer complaints and reviews"></x-canvas-tile>
-    @endcan
-    @auth
-    <x-canvas-tile url="/shipments/requests" title="Shipment Requests" description="Request new shipments and track their progress"></x-canvas-tile>
-    @endauth
-    @canany(['view_basic_server_info', 'view_all_users', 'view_all_roles',
-            'view_detailed_server_info', 'edit_roles'])
-    <x-canvas-tile url="/control-panel" title="Control Panel" description="Manage user roles and access to company resources"></x-canvas-tile>
-    @endrole
-    @if (Route::has('login'))
-      <x-canvas-tile url="/profile" title="Profile" description="Preview your public profile and make changes as needed"></x-canvas-tile>
-    @endif
-    @can('view_employee_count')
-      <x-canvas-tile url="/employeegraph" title="Employee Graph" description=" Visualize the company's employee count over time"></x-canvas-tile>
-    @endcan
-    @auth 
-     <x-canvas-tile url="/faq" title="FAQ" description="Quick answers to common questions"></x-canvas-tile>
-    @endauth
-    @auth 
-     <x-canvas-tile url="/contact" title="Contact" description="Get in touch with us"></x-canvas-tile>
-    @endauth
-    @auth 
-     <x-canvas-tile url="/messages" title="Messages" description="Stay connected with us"></x-canvas-tile>
-    @endauth
-    
+  <div class="flex items-center justify-center">
+    <div
+      class="mx-auto grid grid-cols-4 items-center justify-center gap-8 md:grid-cols-4"
+      style="height: 80vh; width: 80vw;">
+      @auth
+        <x-canvas-tile title="Dashboard" url="/dashboard"
+          description="To the dashboard"></x-canvas-tile>
+      @endauth
+      @can('view_general_employee_content')
+        <x-canvas-tile title="Employee" url="/employee"
+          description="Explore and search for employees in the company"></x-canvas-tile>
+      @endcan
+      @auth
+        <x-canvas-tile url="/shipments/requests" title="Shipment Requests"
+          description="Request new shipments and track their progress"></x-canvas-tile>
+      @endauth
+      @canany(['view_basic_server_info', 'view_all_users', 'view_all_roles',
+        'view_detailed_server_info', 'edit_roles'])
+        <x-canvas-tile url="/control-panel" title="Control Panel"
+          description="Manage user roles and access to company resources"></x-canvas-tile>
+      @endrole
+      @can('view_employee_count')
+        <x-canvas-tile url="/employeegraph" title="Employee Graph"
+          description=" Visualize the company's employee count over time"></x-canvas-tile>
+      @endcan
+      @auth
+        <x-canvas-tile title="Evaluate Shipment Requests"
+          description="Evaluate incoming shipment requests"
+          url="/shipments/requests"></x-canvas-tile>
+      @endauth
+      @auth
+        <x-canvas-tile url="/shipments" title="Confirmed Shipments"
+          description="show confirmed shipments"></x-canvas-tile>
+      @endauth
+      @can('view_reviews')
+        <x-canvas-tile url="/readreviews" title="Customer Complaints"
+          description="Stay informed about all customer complaints and reviews"></x-canvas-tile>
+      @endcan
+      @can('view_all_complaints')
+        <x-canvas-tile url="/contact/manager" title="Problems"
+          description="Respond to questions that the users have"></x-canvas-tile>
+      @endcan
+      @auth
+        <x-canvas-tile url="/contact" title="Contact Us"
+          description="Get in touch with us"></x-canvas-tile>
+      @endauth
+      @auth
+        <x-canvas-tile url="/messages" title="Messages"
+          description="Stay connected with us"></x-canvas-tile>
+      @endauth
+      @auth
+        <x-canvas-tile url="/faq" title="FAQ"
+          description="Quick answers to common questions"></x-canvas-tile>
+      @endauth
+      @if (Route::has('login'))
+        <x-canvas-tile url="/profile" title="Profile"
+          description="Preview your public profile and make changes as needed"></x-canvas-tile>
+      @endif
+    </div>
   </div>
-</div>
 </x-app-layout>
--- resources/views/components/canvas-tile.blade.php
+++ resources/views/components/canvas-tile.blade.php
@@ -1,16 +1,21 @@
-@if(!empty($url))
-  <a href="{{ $url }}" class="bg-blue-100 w-60 h-40 mx-4 my-4 rounded-lg overflow-hidden hover:bg-blue-200 transition-colors duration-200 ease-in-out">
+@if (!empty($url))
+  <a href="{{ $url }}"
+    class="mx-4 my-4 h-40 w-60 overflow-hidden rounded-lg bg-blue-100 transition-colors duration-200 ease-in-out hover:bg-blue-200">
 @endif
-<div class="w-full h-full p-2 flex flex-col justify-center items-center flex-grow flex-shrink">
-@if(!empty($icon))
+<div
+  class="flex h-full w-full flex-shrink flex-grow flex-col items-center justify-center p-2">
+  @if (!empty($icon))
     <div class="mb-2">
-      <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l9-4 9 4v8a4 4 0 01-4 4H7a4 4 0 01-4-4V6z"></path>
+      <svg class="h-8 w-8 text-gray-500" fill="none" stroke="currentColor"
+        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M3 6l9-4 9 4v8a4 4 0 01-4 4H7a4 4 0 01-4-4V6z"></path>
       </svg>
     </div>
-@endif
-  <h2 class="text-lg font-bold mb-2 text-center">{{ $title }}</h2>
-  <h3 class="text-sm font-medium text-gray-600 mb-2 text-center">{{ $description }}</h3>
+  @endif
+  <h2 class="mb-2 text-center text-lg font-bold">{{ $title }}</h2>
+  <h3 class="mb-2 text-center text-sm font-medium text-gray-600">
+    {{ $description }}</h3>
 </div>
 @if (!empty($url))
   </a>
