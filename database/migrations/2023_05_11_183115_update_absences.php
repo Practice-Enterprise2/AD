723435269a733fc996917ca087ed714721bf5131
ParkwayBE <101404183+ParkwayBE@users.noreply.github.com>
2023-05-11 20:33:21 +0200

    commit fix

---
+++ database/migrations/2023_05_11_183115_update_absences.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('absences', function (Blueprint $table) {
+            $table->dropForeign(['contract_id']);
+            $table->foreignId('employee_contract_id')->constrained()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('absences', function (Blueprint $table) {
+            $table->foreignId('employee_contract_id')->change();
+        });
+    }
+};
