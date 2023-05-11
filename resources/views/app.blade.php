96cc217729b2ccac593d1c76bf40baee1c90dfa4
nielsver <nielsvervoort14@gmail.com>
2023-05-11 11:33:32 +0200

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,28 @@
 <x-app-layout>
-  <div class="mx-auto grid grid-cols-4 md:grid-cols-4 gap-80 justify-center" style="height: 400px; width: 800px;">
+<div class="flex items-center justify-center">
+    <div class="mx-auto grid grid-cols-4 md:grid-cols-4 gap-8 justify-center items-center" style="height: 80vh; width: 80vw;">
     @auth
-      <x-canvas-tile title="dashboard" url="/dashboard" description="To the dashboard"></x-canvas-tile>
+      <x-canvas-tile title="dashboard" url="/dashboard" description="To the dashboard" icon="<svg xmlns='http://www.w3.org/2000/svg' class='h-8 w-8 text-gray-500' viewBox='0 0 20 20' fill='currentColor'><path fill-rule='evenodd' d='M2 9h8v8H2V9zm10-7h8v15h-8V2zM4 13h4v4H4v-4z' clip-rule='evenodd' /></svg>" ></x-canvas-tile>
     @endauth
     @can('view_general_employee_content')
-      <x-canvas-tile title="Employee" url="/employee" description="See al the employees in the company"></x-canvas-tile>
+      <x-canvas-tile title="Employee" url="/employee" description="Explore and search for employees in the company"></x-canvas-tile>
     @endcan
     @auth
-      <x-canvas-tile url="/readreviews" title="Read Reviews" description="See all the complaint made by the customers"></x-canvas-tile>
+      <x-canvas-tile url="/readreviews" title="Customer Complaints" description="Stay informed about all customer complaints and reviews"></x-canvas-tile>
     @endauth
     @auth
-    <x-canvas-tile url="/shipments/requests" title="Request a shipment" description="here you can create a new shipment"></x-canvas-tile>
+    <x-canvas-tile url="/shipments/requests" title="Shipment Requests" description="Request new shipments and track their progress"></x-canvas-tile>
     @endauth
+    @canany(['view_basic_server_info', 'view_all_users', 'view_all_roles',
+            'view_detailed_server_info', 'edit_roles'])
+    <x-canvas-tile url="/control-panel" title="Control Panel" description="Manage user roles and access to company resources"></x-canvas-tile>
+    @endrole
+    @if (Route::has('login'))
+      <x-canvas-tile url="/profile" title="Profile" description="Preview your public profile and make changes as needed" icon="<svg xmlns='http://www.w3.org/2000/svg' class='h-8 w-8 text-gray-500' viewBox='0 0 20 20' fill='currentColor'><path fill-rule='evenodd' d='M10 1c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8zm0 2.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5zm0 11c-3.038 0-5.5-2.462-5.5-5.5 0-.971.259-1.878.711-2.664C7.264 5.581 8.59 5 10 5c1.41 0 2.736.581 3.789 1.836.452.786.711 1.693.711 2.664 0 3.038-2.462 5.5-5.5 5.5z' clip-rule='evenodd'/></svg>"></x-canvas-tile>
+    @endif
+    @can('view_employee_count')
+      <x-canvas-tile url="/employeegraph" title="Employee Graph" description=" Visualize the company's employee count over time"></x-canvas-tile>
+    @endcan
   </div>
+</div>
 </x-app-layout>
--- resources/views/components/canvas-tile.blade.php
+++ resources/views/components/canvas-tile.blade.php
@@ -4,7 +4,9 @@
 <div class="w-full h-full p-2 flex flex-col justify-center items-center flex-grow flex-shrink">
 @if(!empty($icon))
     <div class="mb-2">
-      <img class="w-8 h-8 text-gray-500" src="{{ $icon }}">
+      <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l9-4 9 4v8a4 4 0 01-4 4H7a4 4 0 01-4-4V6z"></path>
+      </svg>
     </div>
 @endif
   <h2 class="text-lg font-bold mb-2 text-center">{{ $title }}</h2>
