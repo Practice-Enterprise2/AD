a10da2936be09644cc0afe38c1df0e25dd7b35c6
Mantriq <martinx1606@gmail.com>
2023-05-17 18:26:45 +0200

    requested permissions changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -63,7 +63,8 @@ class AppServiceProvider extends ServiceProvider
         static::bootstrap_permission('view_all_complaints', 'view complaints from customers and handle complaint');
         static::bootstrap_permission('view_employee_count', 'the amount of employees in the company');
         static::bootstrap_permission('view_reviews', 'the reviews of the customers');
-        static::bootstrap_permission('add_edit_vacant_jobs', 'add a vacant job and mark it as filled');
+        static::bootstrap_permission('add_vacant_jobs', 'add a vacant job');
+        static::bootstrap_permission('edit_vacant_jobs', 'mark a vacant job as filled and view the applicants');
 
         // Create the minimum required roles (user groups).
         $role_admin = static::bootstrap_role('admin', 'User group that is granted all permissions. USE WITH CAUTION!');
@@ -82,7 +83,8 @@ class AppServiceProvider extends ServiceProvider
         $role_employee_hr->givePermissionTo('view_all_roles');
         $role_employee_hr->givePermissionTo('edit_any_user_info');
         $role_employee_hr->givePermissionTo('view_all_employees');
-        $role_employee_hr->givePermissionTo('add_edit_vacant_jobs');
+        $role_employee_hr->givePermissionTo('add_vacant_jobs');
+        $role_employee_hr->givePermissionTo('edit_vacant_jobs');
 
         $role_employee_it->givePermissionTo('view_basic_server_info');
         $role_employee_it->givePermissionTo('view_all_permissions');
