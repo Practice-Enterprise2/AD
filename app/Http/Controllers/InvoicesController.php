89e7e7eee9303d7f63bd0bbef426529ff41a076e
ParkwayBE <101404183+ParkwayBE@users.noreply.github.com>
2023-05-18 23:40:30 +0200

    commit name in invoicelist

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -9,12 +9,8 @@ class InvoicesController extends Controller
 {
     public function viewAllInvoices()
     {
-        /* $invoices = array(array("Ben Van Damme","custID1", "generatedUniqueCodeXXXX", "250", "54", "Belgium", "Mechelen", "2800", "Merodestraat 246", "Poland", "Warschau", "54545", "Waschaustreet 20", "15-11-2023", "250", "10/04/2023"),
-        array("Ben Van Damme","custID1", "generatedUniqueCodeXXYY", "450", "75", "Belgium", "Mechelen", "2800", "DeRing 111", "United Kingdom", "Londen", "84484", "Bigstreet 38", "1-12-2023", "450", "23/08/2023"), 
-        array("Jan Janssens","custID2", "generatedUniqueCodeYYYY", "389", "68", "Belgium", "SKW", "2860", "Stationstraat", "Poland", "Warschau", "54545", "Waschaustreet 20", "15-11-2023", "250", "10/04/2023"));
-        return view('invoiceslist', ['invoices' => $invoices]); */
         $invoices = DB::table('invoices')->get();
-
+        
         return view('invoiceslist', ['invoices' => $invoices]);
     }
     public function viewInvoiceDetails(Request $req)
--- resources/views/livewire/invoice-list.blade.php
+++ resources/views/livewire/invoice-list.blade.php
@@ -11,6 +11,8 @@
     <div class="mx-auto">
             <table class="w-[1000px] text-center border-solid border-2">
                 <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
                 <th>Shipment ID</th>
                 <th>Invoice Code</th>
                 <th>Due Date</th>
@@ -21,21 +23,23 @@
                 @if(@isset($invoices))
                     @foreach($invoices as $invoice)
                     <tr class="border-solid border-2">
-                        <td>{{$invoice->id}}</td>
-                        <td>{{$invoice->shipment_id}}</td>
-                        <td>{{$invoice->invoice_code}}</td>
-                        <td>{{$invoice->due_date}}</td>
-                        <td>{{$invoice->total_price}}</td>
-                        <td>{{$invoice->total_price_excl_vat}}</td>
-                        <td>{{$invoice->is_paid}}</td>
-                        <td>{{$invoice->created_at}}</td>
+                        <td>{{$invoice['id']}}</td>
+                        <td>{{$invoice['name']}}</td>
+                        <td>{{$invoice['last_name']}}</td>
+                        <td>{{$invoice['shipment_id']}}</td>
+                        <td>{{$invoice['invoice_code']}}</td>
+                        <td>{{$invoice['due_date']}}</td>
+                        <td>{{$invoice['total_price']}}</td>
+                        <td>{{$invoice['excl_vat']}}</td>
+                        <td>{{$invoice['is_paid']}}</td>
+                        <td>{{$invoice['created_at']}}</td>
                         <td>
                         <form method="post" action="invoices_list/details" accept-charset="UTF-8">
                             @csrf
                         <button type="submit" id="editButton"
                 class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300">Details</button>
             </div>
-                        <input type="hidden" name="invoiceID" value="{{$invoice->id}}">
+                        <input type="hidden" name="invoiceID" value="{{$invoice['id']}}">
                         </form>
                         </td>
                     </tr>
