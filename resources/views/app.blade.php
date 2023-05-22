95c94a041a3deefffd545ba4c1af6ce210cbb843
nielsver <nielsvervoort14@gmail.com>
2023-05-22 15:05:41 +0200

    format

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,9 +56,9 @@
         description="If you want to share your experience, you can leave a review"></x-canvas-tile>
       <x-canvas-tile :url="route('invoice_overview')" title="Invoices"
         description="View your invoices here"></x-canvas-tile>
-      @can('change_employee_contracts') 
-      <x-canvas-tile :url="route('employee-view-contracts')" title="Contracts"
-        description="View All the contracts here"></x-canvas-tile>
+      @can('change_employee_contracts')
+        <x-canvas-tile :url="route('employee-view-contracts')" title="Contracts"
+          description="View All the contracts here"></x-canvas-tile>
       @endcan
     </div>
   </x-content-layout>
--- resources/views/employee_view_contracts.blade.php
+++ resources/views/employee_view_contracts.blade.php
@@ -4,7 +4,7 @@
     <a id="newemployee"
       class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300"
       href="{{ route('contract.index') }}">new contract</a><br><br>
-      <a id="newemployee"
+    <a id="newemployee"
       class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300"
       href="{{ route('contracts.end') }}">determine contract</a><br><br>
     <form action="{{ route('employee-contract-search') }}" method="GET"
