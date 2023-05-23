d928fd4bdbb408be4b49c838848e8378d38cc8e8
ParkwayBE <101404183+ParkwayBE@users.noreply.github.com>
2023-05-23 13:13:36 +0200

    Improve the invoices code
    * overview2
    
    * overviewemployee
    
    * commit1
    
    * commit invoicelist
    
    * commit layout fix
    
    * commit name in invoicelist
    
    * commit cleaned the controller
    
    * commit added mail functionality
    
    * commit format fix
    
    * commit fixed email
    
    * double route deleted 1
    
    * Update web.php
    
    format fix
    
    * commit fixed the requested changes
    
    * format fix and merged with main merge issues resolved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -71,6 +71,7 @@ class AppServiceProvider extends ServiceProvider
         static::bootstrap_permission('delete_any_shipment', 'Delete any of the shipments, regardless of who they belong to.');
         static::bootstrap_permission('accept_any_shipment', 'Accept any shipment for shipping.');
         static::bootstrap_permission('edit_any_employee', 'Edit all of the information on any employee.');
+        static::bootstrap_permission('view_all_invoices', 'View a list of all invoices and be able to resend the invoice email');
         static::bootstrap_permission('change_employee_contracts', 'View the contracts of all employees and be able to create new contracts');
 
         static::bootstrap_permission('view_all_orders', 'View graph order prediction.');
@@ -87,6 +88,7 @@ class AppServiceProvider extends ServiceProvider
         $role_employee->givePermissionTo('view_all_complaints');
         $role_employee->givePermissionTo('edit_all_shipments');
         $role_employee->givePermissionTo('accept_any_shipment');
+        $role_employee->givePermissionTo('view_all_invoices');
 
         $role_employee_hr->givePermissionTo('view_all_users');
         $role_employee_hr->givePermissionTo('edit_roles');
