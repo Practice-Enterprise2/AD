e57463888368532b97c038bc49f1c78a7c946434
Jellios <62890668+Jellios@users.noreply.github.com>
2023-05-24 16:53:51 +0200

    added seeders
    added seeders for the following tables:
    - contracts
    - airlines
    - airports

---
+++ database/seeders/airportTableSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class airportTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('airports')->insert([
+            [
+                'iata_code' => 'LAX',
+                'name' => 'Los Angeles International Airport',
+                'land' => 'United States',
+                'address_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'iata_code' => 'JFK',
+                'name' => 'John F. Kennedy International Airport',
+                'land' => 'United States',
+                'address_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'iata_code' => 'LHR',
+                'name' => 'London Heathrow Airport',
+                'land' => 'United Kingdom',
+                'address_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'iata_code' => 'CDG',
+                'name' => 'Charles de Gaulle Airport',
+                'land' => 'France',
+                'address_id' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'iata_code' => 'HND',
+                'name' => 'Tokyo Haneda Airport',
+                'land' => 'Japan',
+                'address_id' => 5,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
