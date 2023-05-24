439b4711b038907b1ae45eb71259d2ed99c5314f
Jopsakee <Joppefromb@outlook.com>
2023-05-24 18:10:02 +0200

    Final final tweak

--- config/mail.php
+++ config/mail.php
@@ -35,8 +35,8 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'mail.bluesky.local'),
-            'port' => env('MAIL_PORT', 25),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
