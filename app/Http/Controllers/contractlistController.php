758bb343ee66fa341f342b91f905c35f0f09e6c7
guillaume848 <75422493+guillaume848@users.noreply.github.com>
2023-05-24 11:07:45 +0200

    juste search

--- app/Http/Controllers/contractlistController.php
+++ app/Http/Controllers/contractlistController.php
@@ -1,48 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Contract;
-use App\Models\ContractList;
-use Barryvdh\DomPDF\Facade\Pdf;
+use DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\contractList;
+use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\Contract;
 
-class Contractlistcontroller extends Controller
+use Illuminate\View\View;
+
+class contractlistcontroller extends Controller
 {
-    public function contract_pdf($id)
-    {
+    public function contract_pdf($id){
+
         $contract = Contract::find($id);
         $pdf = Pdf::loadView('pdf', compact('contract'));
-
         return $pdf->download('contract.pdf');
     }
-
     public function contractFiltering(Request $request)
     {
         $filter = $request->query('filter');
-        $filter1 = $request->query('filter1');
-        $filter2 = $request->query('filter2');
 
-        if (! empty($filter)) {
+        if (!empty($filter)) {
             $contracts = contractlist::sortable()
-                ->where('id', 'like', '%'.$filter.'%')
-                ->paginate(15);
-        } else {
-            $contracts = contractlist::sortable()
-                ->paginate(15);
-        }
-        if (! empty($filter1)) {
-            $contracts = contractlist::sortable()
-                ->where('depart_location', 'like', '%'.$filter1.'%')
-                ->paginate(15);
-        } else {
-            $contracts = contractlist::sortable()
-                ->paginate(15);
-        }
-        if (! empty($filter2)) {
-            $contracts = contractlist::sortable()
-                ->where('destination_location', 'like', '%'.$filter2.'%')
+                 ->where('id', 'like','%'.$filter.'%')
+                 ->orwhere('depart_airport_id', 'like', '%'.$filter.'%')           
+                 ->orwhere('airline_id', 'like', '%'.$filter.'%')         
+                 ->orwhere('destination_airport_id','like','%'.$filter.'%')         
                 ->paginate(15);
         } else {
             $contracts = contractlist::sortable()
@@ -51,11 +36,9 @@ class Contractlistcontroller extends Controller
 
         return view('contract_list', ['contracts' => $contracts]);
     }
-
     public function index()
     {
-        $contracts = DB::select('select * from contracts c INNER JOIN airports a ON c.depart_location = a.id INNER JOIN airlines al ON c.airline_id = al.id WHERE c.is_active != 0');
-
-        return view('contract_list', ['contracts' => $contracts]);
+       $contracts = DB::select('select * from contracts c INNER JOIN airports a ON c.depart_location = a.id INNER JOIN airlines al ON c.airline_id = al.id');
+       return view('contract_list',['contracts'=>$contracts]);
     }
 }
