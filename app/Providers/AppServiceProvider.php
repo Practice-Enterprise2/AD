6d5a4439135d467a5eab75b72c4b4fce69447b27
nielsver <nielsvervoort14@gmail.com>
2023-05-24 14:27:31 +0200

    permissiontohr

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -99,6 +99,7 @@ class AppServiceProvider extends ServiceProvider
         $role_employee_hr->givePermissionTo('edit_vacant_jobs');
         $role_employee_hr->givePermissionTo('edit_any_employee');
         $role_employee_hr->givePermissionTo('change_employee_contracts');
+        $role_employee_hr->givePermissionTo('view_employee_count');
 
         $role_employee_it->givePermissionTo('view_basic_server_info');
         $role_employee_it->givePermissionTo('view_all_permissions');
