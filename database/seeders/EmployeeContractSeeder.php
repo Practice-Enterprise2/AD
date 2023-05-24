be90c201bcf435b78908e17f38b7e0348cc3cbe4
ParkwayBE <101404183+ParkwayBE@users.noreply.github.com>
2023-05-24 23:56:29 +0200

    commit contract seeder and holiday saldo seeder

---
+++ database/seeders/EmployeeContractSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeContractSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $employees = DB::table('employees')->orderBy('created_at', 'desc')->take(7)->get();
+        $timestamp = date("Y-m-d");
+    
+        $end_date = '2026-01-01';
+        function randomDate($timestamp, $end_date)
+        {
+            $min = strtotime($timestamp);
+            $max = strtotime($end_date);
+            $val = rand($min, $max);
+
+            return date('Y-m-d', $val);
+        }
+        for($i = 0; $i < count($employees); $i++)
+        DB::table('employee_contracts')->insert([
+            'employee_id' => $employees[$i]->id,
+            'start_date' => $timestamp,
+            'end_date' => randomDate($timestamp, $end_date),    
+            'created_at' => now(),
+            'updated_at' => now()        
+        ]);
+        $this->call([
+            HolidaySaldoSeeder::class,
+        ]);
+    }
+}
