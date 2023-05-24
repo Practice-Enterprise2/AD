0dc1d77ff59b6dd20b25c46830552e3461918897
johan525 <95874895+johan525@users.noreply.github.com>
2023-05-24 16:28:51 +0200

    validations new_contract

--- app/Http/Controllers/NewContractController.php
+++ app/Http/Controllers/NewContractController.php
@@ -12,30 +12,27 @@ class NewContractController extends Controller
 {
     public function plaats(Request $request)
     {
-        /* $request->validate([
-             'airlineid' => 'required',
-             'creationdate' => 'required',
-             'expirationdate' => 'required',
-             'price' => 'required|integer',
-             'airportid' => 'required',
-             'departlocation' => 'required',
-             'destinationlocation' => 'required',
-
-         ],
-             [
-                 'airlineid.required' => 'The AirlineID field is required.',
-                 'airportid.required' => 'The AirportID field is required.',
-
-             ]);
-           $test = new Contract;
-           $test->airline_id = $request->airlineid;
-           $test->creationdate = $request->creationdate;
-           $test->expirationdate = $request->expirationdate;
-           $test->price = $request->price;
-           $test->airportID = $request->airportid;
-           $test->departlocation = $request->departlocation;
-           $test->destinationlocation = $request->destinationlocation;
-           return view('new_contract',['data' => $data]);00 */
+        
+        $request->validate([
+            'airlineid' => 'required',
+            'creationdate' => 'required|date|after_or_equal:today',
+            'expirationdate' => 'required|date|after:start_date',
+            'price' => 'required|numeric',
+            'departlocation' => 'required',
+            'destinationlocation' => 'required',
+        ], [
+            'airlineid.required' => 'The AirlineID field is required.',
+            'creationdate.required' => 'The StartDate field is required.',
+            'creationdate.date' => 'The StartDate must be a valid date.',
+            'creationdate.after_or_equal' => 'The Creation Date must be today or in the future.',
+            'expirationdate.required' => 'The EndDate field is required.',
+            'expirationdate.date' => 'The EndDate must be a valid date.',
+            'expirationdate.after' => 'The Expiration Date must be after the Start Date.',
+            'price.required' => 'The Price field is required.',
+            'price.numeric' => 'The Price must be a numeric value.',
+            'departlocation.required' => 'The Departure Location field is required.',
+            'destinationlocation.required' => 'The Destination Location field is required.',
+        ]);
         $test = new Contract;
         $test->airline_id = $request['airlineid'];
         $test->start_date = $request['creationdate'];
