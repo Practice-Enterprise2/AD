bf4b10d27bbae1194b208d3495dd3aa95f6b8151
johan525 <95874895+johan525@users.noreply.github.com>
2023-05-24 20:40:06 +0200

    format

--- database/seeders/airlineSeeder.php
+++ database/seeders/airlineSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -78,4 +76,3 @@ class airlineSeeder extends Seeder
         ]);
     }
 }
-
--- database/seeders/airportTableSeeder.php
+++ database/seeders/airportTableSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-
 
 class airportTableSeeder extends Seeder
 {
